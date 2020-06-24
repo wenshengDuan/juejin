e1197a6f21be6abb2abd40480d7078e67aac2e1d
wensheng <824201954@qq.com>
2020-06-24 15:30:08 +0800

    基于nuxt搭建项目前端，完成开发环境前端代理，axios拦截，部分登录页内容

---
+++ front/pages/register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form" ref="form" :model="form" :rule="rules">
+      <div class="title-container">
+        <img src="/juejin-logo.png" alt="">
+      </div>
+
+      <el-form-item prop="email">
+        <span class="svg-container">
+          <i class="el-icon-mobile"></i>
+        </span>
+        <el-input
+          v-model="form.email"
+          placeholder="请输入邮箱"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'login',
+    data() {
+      return {
+        form: {
+          email: ''
+        },
+        rules: {
+          email: [
+            { required: true, message: '请输入邮箱', trigger: 'blur' },
+            { type: 'email', message: '请输入正确的邮箱格式', trigger: 'blur' }
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ front/plugins/axios.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+let service = axios.create({
+  timeout: 5000,
+  baseURL: '/api'
+})
+
+// @设置请求拦截器, 用于token鉴权 路由跳转
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+ err => {
+  return Promise.reject(err)
+})
+
+//@设置响应拦截器
+service.interceptors.response.use(
+  response => {
+    const { data }  = response
+    return data
+  }
+)
+
+Vue.prototype.$http = service
+
+export const http = service
